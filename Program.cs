6d05a00fe7420abe68f7cdae679ad59b2ecff427
pravalika-prog <68769158+pravalika-prog@users.noreply.github.com>
2020-12-17 19:21:18 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ienumerable
+{
+ public   class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            
+            p.test1(); 
+            p.test2();
+            p.test3();
+            p.test4();
+            }
+        
+       
+           
+                
+            
+         public void test1()
+                    
+        {
+            Console.WriteLine("please enter the name");
+        }
+
+        public void test2()
+        {
+            Console.WriteLine("please enter the age");
+        }  
+   
+
+            }
+        }     
+
+  
---
+++ program2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ienumerable
+{
+  public static  class program2
+    {
+public  static void test3(this Program o)
+        {
+            Console.WriteLine(" my calss is printed");
+        }
+        public static void test4(this Program O)
+        {
+            Console.WriteLine("Method Four: ");
+        }
+
+    }
+    }
+
