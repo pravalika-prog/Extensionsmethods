88cf92d8ab802066640ccc3088604088b2fa52ed
pravalika-prog <68769158+pravalika-prog@users.noreply.github.com>
2020-12-23 18:41:27 +0530

    Add files via upload

---
+++ extensuion1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace linq
+{
+    class extensuion1
+    {
+        public static class StringExtension
+        {
+            public static int GetWordCount(this string inputstring)
+            {
+                if (!string.IsNullOrEmpty(inputstring))
+                {
+                    string[] strArray = inputstring.Split(' ');
+                    return strArray.Count();
+                }
+                else
+                {
+                    return 0;
+                }
+
+            }
+        }
+        class Program
+        {
+        static void Main(string[] args)
+        {
+            string myWord = "Welcome to Dotnet Tutorials Extension Methods Article";
+            int wordCount = myWord.GetWordCount();
+            Console.WriteLine("string : " + myWord);
+            Console.WriteLine("Count : " + wordCount);
+            Console.Read();
+        }
+    }
+}
+}
